39fe04d7083465819acc7128a407b5e869d910c0
Camila Nunes Ladanha <cnunesladanha@gmail.com>
2026-08-02 16:50:51 -0300

    Adiciona projeto Perfil Pet

---
+++ 03-perfil_pet/main.py
@@ -0,0 +1,22 @@
+print("-" * 35)
+print(" Perfil do Pet ")
+print("-" * 35)
+
+nome_pet = "Luna"
+idade_pet = 13
+peso_pet = 32.0
+raca_pet = "Boxer"
+vacinado = True
+possui_mais_pets = True
+quantos_pets = 2
+
+print("Qual o nome do pet? ", nome_pet)
+print("Qual a idade do pet? ",idade_pet)
+print("Qual o peso do pet? ",peso_pet)
+print("Qual a raça do pet? ",raca_pet)
+print("O pet é vacinado? ", vacinado)
+print("Você possui mais pets? ", possui_mais_pets)
+print("Quantos pets você possui? ",quantos_pets)
+print("-" * 35)
+print("Muito obrigada pelas informações!")
+print("-" * 35)
